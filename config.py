238a51a205e1f115b6057283d49d97856723656c
vikingww2 <63012317+vikingww2@users.noreply.github.com>
2022-07-09 10:22:35 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+token = ""
+oauth2_url = ""
+recover_key = ""
+guildid = ""
+roleid = ""
+
+API_ENDPOINT = "https://discord.com/api/v9" #DONT TOUCH
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+BASE_URL = "http://google.com"
+REDIRECT_URI = "http://google.com/callback"
